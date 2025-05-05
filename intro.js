65b4f73fbf2e6eb2993b2ebe08e597dd4f0af0b4
IRERA Josue <twizeyeyesuirerajo@gmail.com>
2025-05-05 10:04:44 +0200

    many change

--- intro.js
+++ intro.js
@@ -1,20 +1,17 @@
-// filepath: c:\Users\User\Desktop\Up Skills tasks\intro-section-with-dropdown-navigation-main\intro.js
 
-// Select the menu and close buttons
 const menuButton = document.querySelector('.menu-button');
 const closeButton = document.querySelector('.close-button');
+const properties = document.querySelector('.main-properties');
 
-// Initially hide the close button
 closeButton.style.display = 'none';
 
-// Add event listener to the menu button
 menuButton.addEventListener('click', () => {
-    menuButton.style.display = 'none'; // Hide the menu button
-    closeButton.style.display = 'block'; // Show the close button
+    menuButton.style.display = 'none'; 
+    closeButton.style.display = 'block'; 
+    properties.style.display = 'flex'
 });
-
-// Add event listener to the close button
 closeButton.addEventListener('click', () => {
-    closeButton.style.display = 'none'; // Hide the close button
-    menuButton.style.display = 'block'; // Show the menu button
+    closeButton.style.display = 'none'; 
+    menuButton.style.display = 'block'; 
+    properties.style.display = 'none'
 });
