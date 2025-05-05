6e84edec418663ff8ae79725dbc753262c289dc2
IRERA Josue <twizeyeyesuirerajo@gmail.com>
2025-05-05 10:54:17 +0200

    click change

--- intro.js
+++ intro.js
@@ -3,8 +3,6 @@ const menuButton = document.querySelector('.menu-button');
 const closeButton = document.querySelector('.close-button');
 const properties = document.querySelector('.main-properties');
 
-closeButton.style.display = 'none';
-
 menuButton.addEventListener('click', () => {
     menuButton.style.display = 'none'; 
     closeButton.style.display = 'block'; 
